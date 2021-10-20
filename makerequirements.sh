b822c3ddef11a811d5a1c2a2d1e4a6f3781efeb8
Michael Moser <moser.michael@gmail.com>
2021-10-21 02:10:18 +0300

    script to create requirements.txt

---
+++ makerequirements.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+
+python3 -m venv req-venv
+source req-venv/bin/activate
+
+pip3 install PyGithub
+pip3 install websocket-client
+pip3 install ruamel.yaml
+
+pip3 freeze >requirement.txt
+
+deactivate req-venv
+rm -rf req-venv
+
+echo "** got requirements.txt **"
